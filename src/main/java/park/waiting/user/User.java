68391571e78f551f9555ed0822f0e1ab01890564
great-park <cksgh1735@gmail.com>
2023-01-28 16:51:00 +0900

    feat: User entity

---
+++ src/main/java/park/waiting/user/User.java
@@ -0,0 +1,40 @@
+package park.waiting.user;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    private String phoneNumber;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    protected User() {}
+
+    protected User(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // static factory method로 사용하기 위해 생성자 lombok 사용 x
+    public static User of(String phoneNumber) {
+        return new User(phoneNumber);
+    }
+
+}
