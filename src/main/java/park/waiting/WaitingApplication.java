9edafaf088a8ba1283e6d22535ae60b4f78bf690
great-park <cksgh1735@gmail.com>
2023-01-28 17:42:46 +0900

    feat: BaseExceptionHandler and error view

--- src/main/java/park/waiting/WaitingApplication.java
+++ src/main/java/park/waiting/WaitingApplication.java
@@ -2,8 +2,10 @@ package park.waiting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class WaitingApplication {
 
---
+++ src/main/java/park/waiting/common/exception/BaseExceptionHandler.java
@@ -0,0 +1,49 @@
+package park.waiting.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import park.waiting.common.constant.ErrorCode;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    // GeneralException이 발생했을 때 대응
+    @ExceptionHandler
+    public ModelAndView general(GeneralException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage()
+                ),
+                status
+        );
+    }
+
+    // 전체 Exception 처리 -> 대응 못한 경우 여기서 대응
+    @ExceptionHandler
+    public ModelAndView exception(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(e)
+                ),
+                status
+        );
+    }
+}
