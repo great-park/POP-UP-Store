2a3c9132a77298bbcae5dd51018099b8709e12d8
great-park <cksgh1735@gmail.com>
2023-01-29 17:29:53 +0900

    feat: base entity - Jpa auditing

---
+++ src/main/java/park/waiting/common/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package park.waiting.common.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private boolean deleted;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public void hide() {
+        this.setDeleted(true);
+    }
+}
