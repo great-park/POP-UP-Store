b237d6cf35cdbeeb4902a2d46ac44e48f5ae8cbe
great-park <cksgh1735@gmail.com>
2023-02-08 16:29:33 +0900

    feat: Posting 작성 로직 구현

--- src/main/java/park/waiting/domain/posting/service/PostingServiceImpl.java
+++ src/main/java/park/waiting/domain/posting/service/PostingServiceImpl.java
@@ -3,10 +3,16 @@ package park.waiting.domain.posting.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import park.waiting.common.constant.ErrorCode;
+import park.waiting.common.exception.GeneralException;
 import park.waiting.domain.posting.dto.PostingRequest;
 import park.waiting.domain.posting.dto.PostingResponse;
+import park.waiting.domain.posting.entity.Posting;
 import park.waiting.domain.posting.repository.PostingRepository;
+import park.waiting.domain.store.entity.Store;
 import park.waiting.domain.store.repository.StoreRepository;
+import park.waiting.domain.user.entity.Customer;
+import park.waiting.domain.user.repository.CustomerRepository;
 
 @RequiredArgsConstructor
 @Service
@@ -14,12 +20,20 @@ public class PostingServiceImpl implements PostingService{
 
     private final PostingRepository postingRepository;
     private final StoreRepository storeRepository;
+    private final CustomerRepository customerRepository;
 
 
     @Transactional
     @Override
     public PostingResponse create(PostingRequest postingRequest) {
-        return null;
+        Store store = storeRepository.findById(postingRequest.getStoreId())
+                .orElseThrow(() -> new GeneralException(ErrorCode.DATA_ACCESS_ERROR));
+        Customer customer = customerRepository.findById(postingRequest.getCustomerId())
+                .orElseThrow(() -> new GeneralException(ErrorCode.DATA_ACCESS_ERROR));
+        Posting posting = Posting.of(customer, store, postingRequest);
+        Posting saved = postingRepository.save(posting);
+
+        return saved.toResponse();
     }
 
     @Transactional
