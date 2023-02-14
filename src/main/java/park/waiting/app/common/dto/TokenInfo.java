f38d43d964c3377ea9b66d76cd38a4445aa98df4
great-park <cksgh1735@gmail.com>
2023-02-14 21:20:50 +0900

    feat: SpringSecurityConfig and Token dto

---
+++ src/main/java/park/waiting/app/common/dto/TokenInfo.java
@@ -0,0 +1,17 @@
+package park.waiting.app.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class TokenInfo {
+
+    private String grantType;
+
+    private String accessToken;
+
+    private String refreshToken;
+}
---
+++ src/main/java/park/waiting/config/SpringSecurityConfig.java
@@ -0,0 +1,10 @@
+package park.waiting.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfiguration {
+}
